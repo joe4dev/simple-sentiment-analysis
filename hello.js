e4db8efde9b4e72695a255187ba5bd7edeef31db
Joel Scheuner <joel.scheuner.dev@gmail.com>
2015-03-15 00:09:19 +0100

    Add simple sentiment analysis ui

--- hello.js
+++ hello.js
@@ -1,11 +1,40 @@
 var port = (process.env.VCAP_APP_PORT || 3000);
 var express = require("express");
+var sentiment = require("sentiment");
 
 var app = express();
 
 app.get('/hello', function(req, res) {
     res.send("Hello world.");
 });
+
+app.get('/testSentiment',
+    function (req, res) {
+        var response = "<HEAD>" +
+          "<title>Twitter Sentiment Analysis</title>\n" +
+          "</HEAD>\n" +
+          "<BODY>\n" +
+          "<P>\n" +
+          "Welcome to the Twitter Sentiment Analysis app.  " +   
+          "What phrase would you like to analzye?\n" +                
+          "</P>\n" +
+          "<FORM action=\"/testSentiment\" method=\"get\">\n" +
+          "<P>\n" +
+          "Enter a phrase to evaluate: <INPUT type=\"text\" name=\"phrase\"><BR>\n" +
+          "<INPUT type=\"submit\" value=\"Send\">\n" +
+          "</P>\n" +
+          "</FORM>\n" +
+          "</BODY>";
+        var phrase = req.query.phrase;
+        if (!phrase) {
+            res.send(response);
+        } else {
+            sentiment(phrase, function (err, result) {
+                response = 'sentiment(' + phrase + ') === ' + result.score;
+                res.send(response);
+            });
+        }
+    });
 
 app.listen(port);
 console.log("Server listening on port " + port);
