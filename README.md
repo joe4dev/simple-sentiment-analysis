90135730540709d6e1feabbd5b584cec0324775d
Joel Scheuner <joel.scheuner.dev@gmail.com>
2015-03-17 20:00:03 +0100

    Add simple UI

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Sentiment Analysis
+
+* Based on tutorial: http://www.ibm.com/developerworks/library/wa-nodejs-app/
+  * The final app doesn't work with the snippets from the online tutorial => Some code is missing!
+* Original source code from Bluemix: https://hub.jazz.net/project/1167/bluemixsentimentanalysis/overview
+* Git repo url: https://hub.jazz.net/git/spirit/Sentiment.Analysis.App
--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 var port = (process.env.VCAP_APP_PORT || 3000);
 var express = require("express");
+var bodyParser = require('body-parser');
 var sentiment = require("sentiment");
 var Twitter = require('twitter');
 var nconf = require("nconf");
@@ -9,9 +10,15 @@ var nconf = require("nconf");
 
 var app = express();
 
-if (app.get('env') == 'development') {
-  app.use(express.errorHandler());
-}
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+process.on('uncaughtException', function (err) {
+    console.error('Caught exception: ' + err.stack);
+});
+process.on("exit", function(code) {
+    console.log("exiting with code: " + code);
+});
 
 app.get('/hello', function(req, res) {
     res.send("Hello world.");
@@ -90,6 +97,152 @@ app.get('/watchTwitter', function (req, res) {
         throw error;
       });
     });
+});
+
+// ===== Full application =====
+var stream;
+var tweetCount = 0;
+var tweetTotalSentiment = 0;
+var monitoringPhrase;
+
+app.get('/sentiment', function (req, res) {
+    res.json({monitoring: (monitoringPhrase != null),
+      monitoringPhrase: monitoringPhrase,
+      tweetCount: tweetCount,
+      tweetTotalSentiment: tweetTotalSentiment,
+      sentimentImageURL: sentimentImage()});
+});
+
+// Example:
+// url -X POST http://localhost:3000/sentiment --data '{"phrase":"obama"}' --header "Content-Type:application/json"
+app.post('/sentiment', function (req, res) {
+  try {
+    if (req.body.phrase) {
+      beginMonitoring(req.body.phrase);
+      res.send(200);
+    } else {
+      res.status(400).send('Invalid request: send {"phrase": "obama"}');
+    }
+  } catch (exception) {
+    res.status(400).send('Invalid request: send {"phrase": "obama"}');
+  }
+});
+
+function resetMonitoring() {
+  if (stream) {
+    var tempStream = stream;
+    stream = null;  // signal to event handlers to ignore end/destroy
+    tempStream.destroySilent();
+  }
+    monitoringPhrase = "";
+}
+
+function beginMonitoring(phrase) {
+    var stream;
+    // cleanup if we're re-setting the monitoring
+    if (monitoringPhrase) {
+        resetMonitoring();
+    }
+    monitoringPhrase = phrase;
+    tweetCount = 0;
+    tweetTotalSentiment = 0;
+    stream = client.stream('statuses/filter', {
+        'track': monitoringPhrase
+      }, function (inStream) {
+        // remember the stream so we can destroy it when we create a new one.
+        // if we leak streams, we end up hitting the Twitter API limit.
+        stream = inStream;
+        console.log("Monitoring Twitter for " + monitoringPhrase);
+        stream.on('data', function (data) {
+          // only evaluate the sentiment of English-language tweets
+          if (data.lang === 'en') {
+            sentiment(data.text, function (err, result) {
+              tweetCount++;
+              tweetTotalSentiment += result.score;
+            });
+          }
+        });
+        stream.on('error', function (error, code) {
+          console.error("Error received from tweet stream: " + code);
+          if (code === 420 || code === 429)  {
+            console.error("API limit hit, are you using your own keys?");
+          }
+          resetMonitoring();
+        });
+        stream.on('end', function (response) {
+          if (stream) { // if we're not in the middle of a reset already
+            // Handle a disconnection
+            console.error("Stream ended unexpectedly, resetting monitoring.");
+            resetMonitoring();
+          }
+        });
+        stream.on('destroy', function (response) {
+          // Handle a 'silent' disconnection from Twitter, no end/error event fired
+          console.error("Stream destroyed unexpectedly, resetting monitoring.");
+          resetMonitoring();
+        });
+      });
+    return stream;
+}
+
+function sentimentImage() {
+    var avg = tweetTotalSentiment / tweetCount;
+    if (avg > 0.5) { // happy
+        return "/images/excited.png";
+    }
+    if (avg < -0.5) { // angry
+        return "/images/angry.png";
+    }
+    // neutral
+    return "/images/content.png";
+}
+
+var DEFAULT_TOPIC = "obama";
+app.get('/',
+    function (req, res) {
+        var welcomeResponse = "<HEAD>" +
+            "<title>Twitter Sentiment Analysis</title>\n" +
+            "</HEAD>\n" +
+            "<BODY>\n" +
+            "<P>\n" +
+            "Welcome to the Twitter Sentiment Analysis app.<br>\n" +
+            "What would you like to monitor?\n" +
+            "</P>\n" +
+            "<FORM action=\"/monitor\" method=\"get\">\n" +
+            "<P>\n" +
+            "<INPUT type=\"text\" name=\"phrase\" value=\"" + DEFAULT_TOPIC + "\"><br><br>\n" +
+            "<INPUT type=\"submit\" value=\"Go\">\n" +
+            "</P>\n" + "</FORM>\n" + "</BODY>";
+        if (!monitoringPhrase) {
+            res.send(welcomeResponse);
+        } else {
+            var monitoringResponse = "<HEAD>" +
+                "<META http-equiv=\"refresh\" content=\"5; URL=http://" +
+                req.headers.host +
+                "/\">\n" +
+                "<title>Twitter Sentiment Analysis</title>\n" +
+                "</HEAD>\n" +
+                "<BODY>\n" +
+                "<P>\n" +
+                "The Twittersphere is feeling<br>\n" +
+                "<IMG align=\"middle\" src=\"" + sentimentImage() + "\"/><br>\n" +
+                "about " + monitoringPhrase + ".<br><br>" +
+                "Analyzed " + tweetCount + " tweets...<br>" +
+                "</P>\n" +
+                "<A href=\"/reset\">Monitor another phrase</A>\n" +
+                "</BODY>";
+            res.send(monitoringResponse);
+        }
+    });
+
+app.get('/monitor', function (req, res) {
+    beginMonitoring(req.query.phrase);
+    res.redirect(302, '/');
+});
+
+app.get('/reset', function (req, res) {
+    resetMonitoring();
+    res.redirect(302, '/');
 });
 
 app.listen(port);
