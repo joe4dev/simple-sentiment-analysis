e2fabe02f80a1a657d6088a654b5355d4685ee3d
Joel Scheuner <joel.scheuner.dev@gmail.com>
2015-03-17 18:26:38 +0100

    Add Twitter API integration

--- hello.js
+++ hello.js
@@ -1,8 +1,17 @@
 var port = (process.env.VCAP_APP_PORT || 3000);
 var express = require("express");
 var sentiment = require("sentiment");
+var Twitter = require('twitter');
+var nconf = require("nconf");
+
+// Alternatively use nconf.get(KEY) to handle config
+//nconf.file({ file: './credentials.json' });
 
 var app = express();
+
+if (app.get('env') == 'development') {
+  app.use(express.errorHandler());
+}
 
 app.get('/hello', function(req, res) {
     res.send("Hello world.");
@@ -35,6 +44,31 @@ app.get('/testSentiment',
             });
         }
     });
+
+// Create your own app at: https://dev.twitter.com/apps
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+app.get('/twitterCheck', function (req, res) {
+    tweeter.verifyCredentials(function (error, data) {
+        res.send("Hello, " + data.name + ".  I am in your twitters.");
+    });
+});
+
+// Alternatively use GET account/verify_credentials to verify credentials
+app.get('/api_limits', function (req, res) {
+  client.get('application/rate_limit_status', function(error, tweets, response){
+    if(error) {
+      console.log(error)
+      throw error;
+    }
+    res.send(response);
+  });
+});
 
 app.listen(port);
 console.log("Server listening on port " + port);
